df660ef4aff3bb9ddcadb463f72f7b2211d39bd5
Ash Blue <ash@blueashes.com>
2014-07-28 08:25:52 -0700

    Live debugging for grid
    Activate debug mode on the pathfinder for a live rebuildable visual
    data feed

--- Assets/Scripts/PathfinderGrid.cs
+++ Assets/Scripts/PathfinderGrid.cs
@@ -18,17 +18,24 @@ public class PathfinderGrid : MonoBehaviour {
 	public GameObject tilePrefab;
 	public LayerMask whatIsCollision;
 
-	// @TODO For debugging purposes we need to be able to re-poll the entire scene every 1 second
+	public bool debug = false; // Allows for live debug mode, not recommended in production mode (expendsive)
+	public float debugRefresh = 1;
+	float debugRefreshCount = 0;
+
 	void Awake () {
+		Init();
+	}
+
+	void Init () {
 		// Get the required size data
 		boundingBox = GameObject.Find("BoundingBox");
 		boxWidth = boundingBox.collider2D.bounds.size.x;
 		boxHeight = boundingBox.collider2D.bounds.size.y;
-
+		
 		float boxX = boundingBox.collider2D.bounds.center.x - (boundingBox.collider2D.bounds.size.x / 2);
 		float boxY = boundingBox.collider2D.bounds.center.y + (boundingBox.collider2D.bounds.size.y / 2);
 		boxPos = new Vector3(boxX, boxY, 0);
-
+		
 		// Generate a grid of tiles
 		grid = new PathfinderTile[Mathf.RoundToInt(boxHeight / tileSize), Mathf.RoundToInt(boxWidth / tileSize)];
 		for (int y = 0, l = GetHeightInTiles(); y < l; y++) {
@@ -39,20 +46,18 @@ public class PathfinderGrid : MonoBehaviour {
 				grid[y, x].Init(x, y, tileSize, boxPos);
 			}
 		}
-	}
 
-	void Start () {
 		// Populate collision data
 		for (int y = 0, l = GetHeightInTiles(); y < l; y++) {
 			for (int x = 0, lX = GetWidthInTiles(); x < lX; x++) {
 				GetTile(x, y).UpdateCollision(whatIsCollision);
 			}
 		}
-
+		
 		// Run clearanc and collision setup here since collision data is ready
 		pathCollision.Init(this);
 		pathClearance.Init(this, pathCollision);
-
+		
 		// Find all valid pathways for gravity based pathfinding
 		List<PathfinderTile> ledges = new List<PathfinderTile>();
 		List<PathfinderTile> corners = new List<PathfinderTile>();
@@ -62,63 +67,63 @@ public class PathfinderGrid : MonoBehaviour {
 				if (tile.cost == 1 && Blocked(x, y + 1) && y + 1 != lY) {	// If the bottom ledge is blocked or bottom of y axis
 					tile.SetLedge(true);
 					ledges.Add(tile);
-
+					
 					if (!Blocked(x - 1, y + 1) || !Blocked(x + 1, y + 1)) 
 						corners.Add(tile);
 				}
 			}
 		}
-
+		
 		// For each ledge, add a link to all ledge neighbors
 		for (int i = 0, l = ledges.Count; i < l; i++) {
 			int x = (int)ledges[i].xy.x;
 			int y = (int)ledges[i].xy.y;
-
+			
 			// Left neighbor check
 			if (!Blocked(x - 1, y)) {
 				PathfinderTile tile = GetTile(x - 1, y);
 				if (tile.ledge) ledges[i].AddLink(tile, 0, 1);
 			}
-
+			
 			// Right neighbor check
 			if (!Blocked(x + 1, y)) {
 				PathfinderTile tile = GetTile(x + 1, y);
 				if (tile.ledge) ledges[i].AddLink(tile, 0, 1);
 			}
 		}
-
+		
 		// Find ledge corner fall points
 		// Raycast at specific angle to check for clear fall point
 		// If valid create a one way link
 		for (int i = 0, l = corners.Count; i < l; i++) {
 			// Discover the direction the tile is facing
 			int direction = Blocked((int)corners[i].xy.x - 1, (int)corners[i].xy.y + 1) ? 1 : -1;
-
+			
 			// Step over the facing direction 1 tile
 			PathfinderTile overhang = GetTile((int)corners[i].xy.x + direction, (int)corners[i].xy.y);
-
+			
 			// Shoot a raycast straight down to the end of the boundary to look for a fall
 			RaycastHit2D hit = Physics2D.Raycast(
 				overhang.transform.position, 
 				-Vector2.up, 
 				(GetHeightInTiles() - overhang.xy.y) * tileSize,
 				whatIsCollision);
-
+			
 			// If we hit something add a fall link at the hit target
 			if (hit.collider) {
 				PathfinderTile tileTarget = GetTileByPos(hit.point);
 				int distance = (int)Mathf.Floor(Vector3.Distance(corners[i].transform.position, tileTarget.transform.position));
 				corners[i].AddLink(tileTarget, 1, distance, "fall");
 			}
-
+			
 			// Find corner runoff point
 			hit = Physics2D.Raycast(
 				overhang.transform.position,
 				new Vector2(0.2f * direction, -0.5f),
 				(GetHeightInTiles() - overhang.xy.y) * tileSize,
 				whatIsCollision
-			);
-
+				);
+			
 			// If valid create a 2 way link
 			if (hit.collider) {
 				PathfinderTile tileTarget = GetTileByPos(hit.point);
@@ -127,11 +132,31 @@ public class PathfinderGrid : MonoBehaviour {
 				tileTarget.AddLink(corners[i], 1, distance, "runoff"); // @TODO Should probably be runoff jump
 			}
 		}
-
+		
 		// From all ledge corners calculate jump parabola and link if successful
 		// Check for clearance by fudging an extra jump arc above to simulate clearance
 		// Needs to be a real physics jump arc
 		// Raycast along the jump arc for collision tests
+	}
+
+	// Reset pathfinder back to ground zero
+	void Reset () {
+		foreach (Transform child in transform) {
+			Destroy(child.gameObject);
+		}
+	}
+
+	void FixedUpdate () {
+		// If debug mode is active, update the grid every 60 seconds
+		if (!debug) return;
+		
+	    if (debugRefreshCount > debugRefresh * 60) {
+			Reset();
+			Init();
+			debugRefreshCount = 0;
+		}
+
+		debugRefreshCount += 1;
 	}
 
 	// Turn real world coordinates into a tile postiion
